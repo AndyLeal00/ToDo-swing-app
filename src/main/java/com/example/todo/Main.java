4572c32e9833216d3719b58221dfe9c5b4828024
AndyLeal00 <46579163+AndyLeal00@users.noreply.github.com>
2026-04-27 12:05:34 -0500

    Add files via upload

--- src/main/java/com/example/todo/Main.java
+++ src/main/java/com/example/todo/Main.java
@@ -3,7 +3,7 @@ package com.example.todo;
 import javax.swing.SwingUtilities;
 
 /**
- * Punto de entrada de la aplicación.
+ * Punto de entrada de la aplicacion.
  */
 public class Main {
     public static void main(String[] args) {
